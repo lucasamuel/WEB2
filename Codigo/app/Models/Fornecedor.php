8c6f51876c96519247f05c5616d2c646445803ad
Lucas Samuel <lucas.samuel@fortbrasil.com.br>
2021-10-13 08:56:14 -0300

    Organizando branch de códigos

--- Codigo/app/Models/Fornecedor.php
+++ Codigo/app/Models/Fornecedor.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = "fornecedor";
+
+    protected $filable = ['descricao', 'endereco' ]
 }
